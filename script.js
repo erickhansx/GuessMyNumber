e019d1205478a75c41ec5f2a838e8bb1bd147789
erickhansx <50700379+erickhansx@users.noreply.github.com>
2022-06-30 11:32:19 -0500

    Add background color style for when the player wins

--- script.js
+++ script.js
@@ -16,10 +16,17 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
 
+  //When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔No number!';
+
+    // When the player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number 👏';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High! 📈';
@@ -28,6 +35,8 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
+
+      //When guess is too low
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
