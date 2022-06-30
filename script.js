d3363ac9f9ffc12883890f0d1c08a5c91bf82b65
erickhansx <50700379+erickhansx@users.noreply.github.com>
2022-06-30 11:35:36 -0500

    Change the width of the number box when player wins using DOM

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct Number 👏';
 
     document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
 
     //When guess is too high
   } else if (guess > secretNumber) {
